43f1b6d3c0847354cf0fac08f824ca7f3daa3ebc
Jukka Kurkela <jukka.kurkela@gmail.com>
2022-12-12 06:37:07 +0200

    feat: add types

---
+++ types/index.esm.d.ts
@@ -0,0 +1,28 @@
+import { Chart, ChartType } from 'chart.js';
+
+declare module 'chart.js' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface PluginOptionsByType<TType extends ChartType> {
+    autocolors?: AutocolorsOptions
+  }
+}
+
+interface ColorsDescriptor {
+  background: string
+  border: string
+}
+
+interface AutocolorsOptions {
+  enabled?: boolean,
+  mode?: 'dataset' | 'data',
+  offset?: number,
+  repeat?: number,
+  customize?: (ctx: AutocolorsContext, options: AutocolorsOptions) => ColorsDescriptor
+}
+
+interface AutocolorsContext {
+  chart: Chart
+  colors: ColorsDescriptor
+  dataIndex?: number
+  datasetIndex: number
+}
